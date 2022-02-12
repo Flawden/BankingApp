fca4526c74720d128a6dbe9c660052758700a779
Flawden <topchiidv1998@gmail.com>
2022-02-12 09:32:09 +0300

    Update 2

--- src/BankMenu.java
+++ src/BankMenu.java
@@ -1,3 +1,13 @@
+import data.User;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BankMenu {
 
     private Bank bank;
@@ -7,6 +17,113 @@ public class BankMenu {
     }
 
     public void showStartMenu() {
+        int answer = 0;
+
+        System.out.println("Registration and authorization panel:");
+        System.out.println("Select one: \n" +
+                "1. Login\n" +
+                "2. Register\n" +
+                "3. Exit\n");
+
+        while (true) {
+            try(BufferedReader rd = new BufferedReader(new InputStreamReader(System.in))) {
+                answer = Integer.parseInt(rd.readLine());
+                break;
+            } catch (IOException e) {
+                System.out.println("Error. You should have entered a number");
+            }
+        }
+
+        if(answer == 0 || answer == 3) {
+            System.exit(0);
+        } else if(answer == 1) {
+            showLogin();
+        } else if(answer == 2) {
+            showRegister();
+        }
+    }
+
+    public void showBankMenu() {
+        System.out.println("Select one: \n" +
+                "1. Show my info\n" +
+                "2. Create Loan\n" +
+                "3. Create Debit card\n" +
+                "4. Exit");
+    }
+
+    private void showLogin() {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+        String email = "";
+        String password = "";
+        boolean isCorrect = false;
+
+        while (isCorrect == false) {
+            while (true) {
+                try {
+                    System.out.println("Enter your email");
+                    email = rd.readLine();
+                    System.out.println("Enter your password");
+                    password = rd.readLine();
+                    break;
+                }catch (IOException e) {
+                    System.out.println("Invalid value");
+                }
+            }
+
+            isCorrect = doLogin(email, password);
+            if (isCorrect == false) {
+                System.out.println("Incorrect login or password. Please, try again.");
+            }
+        }
+
+    }
+
+    private void showRegister() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("DD.MM.YYYY");
+        String firstName = "";
+        String lastName = "";
+        String eMail = "";
+        String password = "";
+        Date birthdate;
+        boolean gender = false;
+
+        while (true) {
+            try(BufferedReader rd = new BufferedReader(new InputStreamReader(System.in))) {
+                System.out.println("Enter your name");
+                firstName = rd.readLine();
+
+                System.out.println("Enter your last name");
+                lastName = rd.readLine();
+
+                System.out.println("Enter your E-mail");
+                eMail = rd.readLine();
+
+                System.out.println("Enter your password");
+                password = rd.readLine();
+
+                System.out.println("Enter your birthday date. DD.MM.YYYY");
+                birthdate = dateFormat.parse(rd.readLine());
+
+                System.out.println("Enter your Gender (0 for Male, 1 for Female)");
+                gender = Boolean.parseBoolean(rd.readLine());
+
+                break;
+
+            } catch (Exception e) {
+                System.out.println("Invalid value");
+            }
+        }
+
+        User user = new User();
+
+    }
+
+    private boolean doLogin(String email, String password) {
+        return false;
+    }
+
+    private void doRegister(User user) {
 
     }
 
